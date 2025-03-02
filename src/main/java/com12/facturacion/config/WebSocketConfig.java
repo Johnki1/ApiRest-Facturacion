03652e0d1f3c308ab66c4a449da880a18726a38c
Johnki <garciakider@gmail.com>
2025-03-02 17:48:01 -0500

    edit cors

--- src/main/java/com12/facturacion/config/WebSocketConfig.java
+++ src/main/java/com12/facturacion/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5175");
+        registry.addEndpoint("/wss")
+                .setAllowedOrigins("https://unoigualados.up.railway.app");
     }
 }
